07049c95fba8d34e4240fe46ee88b6817f45e213
Edwin Soria <68561804+bossundeveloper258@users.noreply.github.com>
2022-01-05 17:01:42 -0500

    databse

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tribune_admin/';
+$config['base_url'] = 'http://165.227.43.42/';
 
 
 /*
